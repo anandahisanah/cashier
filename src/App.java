8b47547fa3ce5323af719a4214c3c49ce37d6451
anandahisanah <nurhisanah@gmail.com>
2022-07-11 17:16:22 +0800

    feat: transaction change

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int nomor_menu, jumlah_pesanan, diskon, subtotal, total, pembayaran, kembalian;
@@ -31,9 +32,9 @@ public class App {
         System.out.println("");
 
         // Memilih Menu
-        System.out.print("Masukkan Nomor Menu yang Dipesan : ");
+        System.out.println("________________________________________________");
+        System.out.print ("Masukkan Nomor Menu yang Dipesan : ");
         nomor_menu = input.nextInt();
-        System.out.println("_______________________________________________");
         
         // Memasukkan Jumlah Menu
         System.out.print("Masukkan Jumlah Pesanan : ");
@@ -46,14 +47,88 @@ public class App {
         switch (nomor_menu) {
             case 1:
                 subtotal = 25000 * jumlah_pesanan;
-                System.out.println("1. Milk Vannila Float - Rp.25.000");
+                System.out.println("Milk Vannila Float : Rp.25.000");
                 System.out.println("Total : Rp." + subtotal);
                 break;
+            
             case 2:
                 subtotal = 25000 * jumlah_pesanan;
-                System.out.println("2. Taro Float - Rp.25.000");
+                System.out.println("Taro Float : Rp.25.000");
                 System.out.println("Total : Rp." + subtotal);
                 break;
+            
+            case 3:
+                subtotal = 25000 * jumlah_pesanan;
+                System.out.println("Red velvet Float : Rp.25.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 4:
+                subtotal = 25000 * jumlah_pesanan;
+                System.out.println("Chocolate Float : Rp.25.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 5:
+                subtotal = 25000 * jumlah_pesanan;
+                System.out.println("Mocha Latte Art : Rp.25.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 6:
+                subtotal = 25000 * jumlah_pesanan;
+                System.out.println("Cappucino Latte Art : Rp.25.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 7:
+                subtotal = 15000 * jumlah_pesanan;
+                System.out.println("Ice Lychee Tea : Rp.15.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 8:
+                subtotal = 15000 * jumlah_pesanan;
+                System.out.println("Ice Lemon Tea : Rp.15.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 9:
+                subtotal = 5000 * jumlah_pesanan;
+                System.out.println("Air Putih : Rp.5.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 10:
+                subtotal = 15000 * jumlah_pesanan;
+                System.out.println("Kentang Goreng : Rp.15.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 11:
+                subtotal = 15000 * jumlah_pesanan;
+                System.out.println("Nugget : Rp.15.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 12:
+                subtotal = 15000 * jumlah_pesanan;
+                System.out.println("Sosis : Rp.15.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 13:
+                subtotal = 15000 * jumlah_pesanan;
+                System.out.println("Singkong Balado : Rp.15.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+            
+            case 14:
+                subtotal = 55000 * jumlah_pesanan;
+                System.out.println("Mix Jajanan Paket murah : Rp55.000");
+                System.out.println("Total : Rp." + subtotal);
+                break;
+
             default:
                 subtotal = 0;
                 break;
@@ -73,13 +148,13 @@ public class App {
         System.out.println("_______________________________________________");
 
         // Memasukkan Pembayaran
-        System.out.print("Masukkan Pembayaran : ");
+        System.out.print("Masukkan Pembayaran : Rp.");
         pembayaran = input.nextInt();
         System.out.println("_______________________________________________");
 
         // Menampilkan Kembalian
         kembalian = pembayaran - total;
-        System.out.println("Kembalian : " + kembalian);
+        System.out.println("Kembalian : Rp." + kembalian);
         System.out.println("_______________________________________________");
     }
 }
